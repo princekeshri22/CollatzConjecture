a62fd91c8934c46e20dbe36741e33cce5f81091c
Adrita Chakraborty <110276522+AD-2409@users.noreply.github.com>
2022-10-05 12:05:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 # The Collatz conjecture is a conjecture in mathematics that concerns sequences defined as follows: start with any positive integer n. Then each term is obtained from the previous term as follows:
 
-# If the previous term is even, the next term is half of the previous term.
-# If the previous term is odd, the next term is 3 times the previous term plus 1.
+# If the previous term is even,the next term is half of the previous term.
+# If the previous term is odd,the next term is 3 times the previous term plus 1.
 # The conjecture is that no matter what value of n, the sequence will always reach 1.
 
 # if x is odd then next term 3*x + 1
